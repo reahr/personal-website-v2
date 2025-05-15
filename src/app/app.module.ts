6a8d51639d47ebaf303fa88b62464efaa4e009d4
Reah Rajmangal <reah.rajmangal@relpro.com>
2025-05-15 10:57:43 -0400

    add prerender

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
